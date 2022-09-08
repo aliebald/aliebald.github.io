ab96003d03c473c72f63dfe03af6f2bd4459435a
Alexander Liebald <alexander.liebald@gmail.com>
2022-09-08 21:06:56 +0200

    Add sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: "https://liebald.dev",
+	generateIndexSitemap: false,
+	generateRobotsTxt: false,
+	priority: false,
+	changefreq: false,
+};
